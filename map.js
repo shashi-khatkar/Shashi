2374e288ae5a693150548b2edadbeae34c3b9b10
shashi-khatkar <khatkarshashi949@gmail.com>
2025-07-15 11:08:02 +0530

    third

--- map.js
+++ map.js
@@ -1,12 +1,15 @@
-// const fruits = new Map([
-//   ["apples", 500],
-//   ["bananas", 300],
-//   ["oranges", 200]
-// ]);
-// console.log(fruits.get("apples")); // 500
-// fruits.set("grapes", 400);
-// console.log(fruits.get("grapes")); // 400
+const fruits1 = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
+console.log(fruits1.get("apples")); // 500
+fruits.set("grapes", 400);
+console.log(fruits1.get("grapes")); // 400
 let fruits = new Map();
 fruits.set("shasi",1);
 fruits.set("ritik",2);
 console.log(fruits.get("shasi")); 
+const c=[1,2,3,4,5]
+let results=c.flatMap(x=>[x,x*2])
+console.log(results)
